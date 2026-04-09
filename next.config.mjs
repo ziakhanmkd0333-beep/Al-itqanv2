1b37819cff0eb91bf16d5fa26c9e35cde695aa03
zia <your@email.com>
2026-04-09 11:28:40 +0500

    Changes to be committed: #	modified:   next.config.mjs #

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,7 +13,14 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     webpackBuildWorker: true
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
